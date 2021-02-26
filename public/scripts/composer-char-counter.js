ce62b0d505067ad9bf9e3b68efb0ae016f661847
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-26 04:53:09 +0000

    removed unnecessary comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,12 @@
-// console.log("does this work");
-
 $(document).ready(function() {
-  //  console.log($( "#tweet-text" ))
   $("#tweet-text").on('keyup',onKeyUp);
 });
 
+//Counts characters in the textarea. Character length will determine the css styling//
 const onKeyUp = function() {
   const text = $(this).val();
   const count = text.length;
-  // console.log("the count is: ", count);
   const charLeft = 140 - count;
-  // console.log("chars left are:", charLeft);
-  // //Travel through the DOM to find counter
   let charCounter = $(this).parent().parent().find(".counter");
   $(charCounter).text(charLeft);
   if (charLeft < 0) {
@@ -20,12 +15,4 @@ const onKeyUp = function() {
     charCounter.removeClass("overcount");
   }
 };
-
-
-
-
-//jquery call with class
-
-//jquery. grabbing a part of the doc and either doing something to it or read something from it.
-//select, then change or read
 
